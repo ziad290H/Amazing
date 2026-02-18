cd1d6034c837f1ffaf5c307dd48209ae404cfd54
ziad290H <ziaddaouari10@gmail.com>
2026-02-18 17:40:43 +0100

    push

--- mazegenerator.py
+++ mazegenerator.py
@@ -14,6 +14,15 @@ def validate_params(entry, exit):
     return entry == MazeGenerator.self.entry and exit == MazeGenerator.self.exit
 
 class MazeGenerator:
+    DIRECTIONS = {
+    "N": (0, -1, 1), # (dx, dy, current_bit)
+    "E": (1, 0, 2),
+    "S": (0, 1, 4),
+    "W": (-1, 0, 8)
+    }
+
+    OPPOSITE = {1: 4, 2: 8, 4: 1, 8: 2}
+
     def __init__(self, width: int, height: int, seed: Optional[int] = None):
         self.width = width
         self.height = height
@@ -22,17 +31,19 @@ class MazeGenerator:
         self.entry: Tuple[int, int] = (0, 0)
         self.exit: Tuple[int, int] = (width - 1, height - 1)
     
-    
     def in_bounds(self, x: int, y: int) -> bool:
         return (0 <= x < self.width and 0 <= y < self.height)
         """this methode helps to prevent the out of index eurror"""
     
     def get_neighbors(self, x: int, y: int) -> list[Tuple[int, int, int]]:
-        self.valid_nighbors = []
-        if not in_bounds(self.x, self.y):
-            return None
-        valid_nighbors.append((x, y,))
-        
+        valid_nighbors = []
+        for dx, dy, bit in self.DIRECTIONS.values():
+            nx, ny = x + dx, y + dy
+            
+            if self.in_bounds(nx, ny):
+                #i should add here the verification if the cell is visited
+                valid_nighbors.append(nx, ny, bit)
+        return valid_nighbors        
 
     def generate(self, perfect: bool = True) -> None:
         pass
